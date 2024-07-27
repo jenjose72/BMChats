7cd3b77447c87207666d2545258d002175ec5ad4
jenjose72 <jenjose72@gmail.com>
2024-07-27 22:12:17 +0530

    production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import { app,server } from './socket/socket.js';
@@ -15,18 +16,22 @@ import connectToMongoDB from './db/connectToMongoDb.js';
 
 dotenv.config();
 const PORT=process.env.PORT || 8000;
+const __dirname=path.resolve()
 
 app.use(express.json());
 app.use(cookieParser());
-app.get('/',(req,res)=>{
-    res.send('Hello World!');
-})
 
 app.use('/api/auth',authRoutes);
 
 app.use('/api/messages',messageRoutes);
 
 app.use('/api/users',userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/frontend/dist/index.html"));  // serve the single page application
+})
 
 server.listen(PORT,async ()=>{
     await connectToMongoDB();
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import MessageContainer from '../../components/messages/MessageContainer.jsx'
 
 const Home = () => {
   return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+		<div className='flex sm:h-[100%] md:h-[100%] md:w-[100%] sm:w-[100%] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 			<Sidebar />
 			<MessageContainer />
 		</div>
