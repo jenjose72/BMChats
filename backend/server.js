d614d74097d76ffc5332cebfaad3468ae099292b
jenjose72 <jenjose72@gmail.com>
2024-07-28 08:49:51 +0530

    updated one wrong casing

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 
-import connectToMongoDB from './db/connectToMongoDb.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 
 
